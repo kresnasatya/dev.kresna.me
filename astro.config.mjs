503280053bcdc4af91c50640a289cbfdd7f4ba30
Kresna Satya <kresnasatya72@gmail.com>
2024-09-27 20:02:26 +0800

    add mdx and starlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
+import starlight from "@astrojs/starlight";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://dev.kresna.me"
+  site: "https://dev.kresna.me",
+  integrations: [starlight({
+    title: "dev.kresna.me"
+  }), mdx()]
 });
