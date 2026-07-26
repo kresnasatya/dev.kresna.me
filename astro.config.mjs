2843a40e86e7416b212f99df0cf42847774e1fd5
Kresna Satya <halo@kresna.me>
2026-07-26 18:23:23 +0800

    chore: upgrade to astro 7

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 import { defineConfig } from 'astro/config';
-import rehypeCodeGroup from 'rehype-code-group';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://dev.kresna.me",
   markdown: {
-    rehypePlugins: [
-      rehypeCodeGroup
-    ],
     shikiConfig: {
       themes: {
         light: 'monokai',
